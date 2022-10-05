d0816519fea5fd6ec568364a20107bf22156f26e
Robby0501 <robbyulrich@hotmail.com>
2022-10-05 15:44:50 -0400

    heroku deploy draft

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,13 @@ import styled from "styled-components";
 import { useGetMLBQuery } from "../src/redux/slices/apiSlice";
 
 const Container = styled.div``;
-
+const BetsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 2em;
+    justify-content: center;
+    align-items: center;
+`;
 export default function Home() {
   const { data, isSuccess } = useGetMLBQuery();
 
@@ -11,5 +17,11 @@ export default function Home() {
     isSuccess && console.log(data);
   }, [isSuccess, data]);
 
-  return <Container>YO!!! LMFAOOO @_@</Container>;
+  return <BetsContainer>
+       {isSuccess && data.map(game => 
+       <div>
+        {game.HomeTeam} vs {game.AwayTeam}
+       </div>
+        )}
+  </BetsContainer>;
 }
