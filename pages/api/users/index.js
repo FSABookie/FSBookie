e2daa0490b6505051fb3e8e94744200721a47958
Daniel Margolin <dannyjmargolin@gmail.com>
2022-10-05 15:47:06 -0400

    t

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Order, User } from ".../../../server/db";
+import { Order, User } from "../../../server/db";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
