8e53d1b84229ea8b9aec9770d4dbe52d83266f55
Daniel Margolin <dannyjmargolin@gmail.com>
2022-10-06 12:39:52 -0400

    header

---
+++ server/seedData/bets.js
@@ -0,0 +1,11 @@
+bets = [
+    {
+        eventId: "68866b8f-a99a-433c-a547-bd094f4cb43a"
+    },
+    {
+        eventId: "e8eddc6e-185d-43f1-bba4-f84058e4483c"
+    },
+    {
+        eventId: "9c998eed-12dd-4241-9877-e8a2dafa5ad7"
+    },
+];
