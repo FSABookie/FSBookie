316fd155d51d9d0553a1bd33e7f00ce9357aac65
Daniel Margolin <dannyjmargolin@gmail.com>
2022-10-06 17:07:42 -0400

    link

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ const MainContainer = styled.div`
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
-    <SessionProvider session={session} basePath="/">
+    <SessionProvider session={session} basePath="capstone-bookie.herokuapp.com">
       <Provider api={apiSlice} store={store}>
         <MainContainer>
           <Header />
