21421042b5cb0412baaebb0ccad69b56aa666125
Daniel Margolin <dannyjmargolin@gmail.com>
2022-10-06 00:04:09 -0400

    auth

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -12,6 +12,7 @@ export default async function handler(req, res) {
 	}
 	if (req.method === 'POST') {
 		try {
+			console.log('BODY', req.body);
 			const user = await User.authenticate(req.body);
 			if (!user) res.sendStatus(404);
 			const token = await user.generateToken();
