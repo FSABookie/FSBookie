e40e62620cda27beb50f57ac2ec7c839584ddb13
Daniel Margolin <dannyjmargolin@gmail.com>
2022-10-06 17:13:09 -0400

    link

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,32 @@
-import "../styles/globals.css";
-import styled from "styled-components";
-import { Provider } from "react-redux";
-import { apiSlice } from "../src/redux/slices/apiSlice";
-import { store } from "../src/redux/store/store";
-import { SessionProvider } from "next-auth/react"
-import Header from "../src/components/Header";
+import '../styles/globals.css';
+import styled from 'styled-components';
+import { Provider } from 'react-redux';
+import { apiSlice } from '../src/redux/slices/apiSlice';
+import { store } from '../src/redux/store/store';
+import { SessionProvider } from 'next-auth/react';
+import Header from '../src/components/Header';
 
 const MainContainer = styled.div`
-  position: relative;
-  min-height: 100vh;
-  padding-bottom: 2em;
-  * {
-    margin: 0;
-    user-select: none;
-  }
+	position: relative;
+	min-height: 100vh;
+	padding-bottom: 2em;
+	* {
+		margin: 0;
+		user-select: none;
+	}
 `;
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }) {
-  return (
-    <SessionProvider session={session} basePath="capstone-bookie.herokuapp.com">
-      <Provider api={apiSlice} store={store}>
-        <MainContainer>
-          <Header />
-          <Component {...pageProps} />
-        </MainContainer>
-      </Provider>
-    </SessionProvider>
-  );
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+	return (
+		<Provider api={apiSlice} store={store}>
+			<SessionProvider session={session}>
+				<MainContainer>
+					<Header />
+					<Component {...pageProps} />
+				</MainContainer>
+			</SessionProvider>
+		</Provider>
+	);
 }
 
 export default MyApp;
