0a1e8a69d54b30950b6e8dbd745ba15de15922ab
Daniel Margolin <dannyjmargolin@gmail.com>
2022-10-05 23:04:04 -0400

    stuff

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-export default NextAuth = ({
+export default NextAuth({
     pages: {
         signIn: "/login"
     },
