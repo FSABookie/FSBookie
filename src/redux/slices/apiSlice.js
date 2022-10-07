517755b4450be80043c4e335ed849bd598716a7c
Danny Laptop <dannyjmargolin@gmail.com>
2022-10-07 12:47:16 -0400

    url

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "apiSlice",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.BASE_URL || "http://localhost:3000/" + "api",
+    baseUrl: (process.env.BASE_URL ? process.env.BASE_URL : "http://localhost:3000/") + "api",
   }),
   tagTypes: ["MLB", "NBA", "NFL", "NHL"],
   endpoints: (builder) => ({
