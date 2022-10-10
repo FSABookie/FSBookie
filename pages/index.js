7a095deb509dbf25105dd061af7ee833d0eacf4f
Daniel Margolin <dannyjmargolin@gmail.com>
2022-10-10 15:15:59 -0400

    fixes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useGetMLBQuery } from "../src/redux/slices/apiSlice";
 import LandingPage from "../src/components/LandingPage";
+import { useSession } from "next-auth/react";
+
+
 const Container = styled.div``;
 const BetsContainer = styled.div`
 	display: flex;
