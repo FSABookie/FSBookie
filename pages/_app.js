59d997f400b512b28a29815fc2419e3b3d7db288
byoon97 <byoon5397@gmail.com>
2022-10-13 16:44:27 -0400

    redux persist for game only

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,28 @@
-import styled from 'styled-components';
-import { Provider } from 'react-redux';
-import { apiSlice } from '../src/redux/slices/apiSlice';
-import { store } from '../src/redux/store/store';
-import { SessionProvider } from 'next-auth/react';
-import Layout from '../src/components/Layout';
+import styled from "styled-components";
+import { Provider } from "react-redux";
+import { apiSlice } from "../src/redux/slices/apiSlice";
+import { store, persistor } from "../src/redux/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { SessionProvider } from "next-auth/react";
+import Layout from "../src/components/Layout";
 import "../styles/globals.css";
 
-const Container = styled.div `
-    margin: 0;
-	
+const Container = styled.div`
+  margin: 0;
 `;
 
-
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-	return (
-		<Provider api={apiSlice} store={store}>
-			<SessionProvider session={session}>
-				<Container>
-					<Layout Component={Component} pageProps={pageProps} />
-				</Container>
-			</SessionProvider>
-		</Provider>
-	);
+  return (
+    <Provider api={apiSlice} store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SessionProvider session={session}>
+          <Container>
+            <Layout Component={Component} pageProps={pageProps} />
+          </Container>
+        </SessionProvider>
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default MyApp;
