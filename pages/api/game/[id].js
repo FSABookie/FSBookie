5534e6f6617cf65f28aa7134813203416e4e9c34
byoon97 <byoon5397@gmail.com>
2022-10-18 17:11:23 -0400

    able to check bets for half and quarter

--- pages/api/game/[id].js
+++ pages/api/game/[id].js
@@ -1,14 +1,11 @@
 export default async function handler(req, res) {
   try {
     const { id } = req.query;
-    let response = await fetch(
-      `https://jsonodds.com/api/results/getbyeventid/${id}`,
-      {
-        headers: {
-          "x-api-key": process.env.APIKEY,
-        },
-      }
-    );
+    let response = await fetch(`https://jsonodds.com/api/results/${id}`, {
+      headers: {
+        "x-api-key": process.env.APIKEY,
+      },
+    });
     response = await response.json();
     res.status(200).json(response);
   } catch (error) {
