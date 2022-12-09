2150673b780db355b35dc40be5841d94f9229717
Danny <dannyjmargolin@gmail.com>
2022-12-09 13:43:06 -0500

    url and pg

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const pg = require('pg');
+
 
 const config = {};
 
@@ -6,20 +8,36 @@ if (process.env.QUIET) {
 	config.logging = false;
 }
 
-const db = new Sequelize(
-	process.env.DATABASE_URL || 'postgres://localhost:5432/bookie',
-	{
-		dialect: 'postgres',
-		dialectOptions: {
-			ssl: process.env.DATABASE_URL
-				? {
-						require: true,
-						rejectUnauthorized: false,
-				  }
-				: false,
+// const db = new Sequelize(
+// 	process.env.DATABASE_URL || 'postgres://localhost:5432/bookie',
+// 	{
+// 		dialect: 'postgres',
+// 		dialectOptions: {
+// 			ssl: process.env.DATABASE_URL
+// 				? {
+// 						require: true,
+// 						rejectUnauthorized: false,
+// 				  }
+// 				: false,
+// 		},
+// 		logging: false,
+// 	}
+// );
+
+const db = new Sequelize({
+	database: 'fsbookie',
+	username: 'postgres',
+	password: 'password',
+	host: 'fsbookie.czw8tftookde.us-east-1.rds.amazonaws.com',
+	port: 5432,
+	dialect: 'postgres',
+	dialectModule: pg,
+	dialectOptions: {
+		ssl: {
+			require: true, // This will help, but cause error
+			rejectUnauthorized: false, // This will fix error
 		},
-		logging: false,
-	}
-);
+	},
+});
 
 module.exports = db;
