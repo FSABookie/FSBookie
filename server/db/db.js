516ff12de2548ede9f4586b3769aa3634948cb75
Danny <dannyjmargolin@gmail.com>
2023-03-12 18:23:49 -0400

    migrate to supabase env

--- server/db/db.js
+++ server/db/db.js
@@ -7,36 +7,20 @@ if (process.env.QUIET) {
     config.logging = false;
 }
 
-// const db = new Sequelize(
-// 	process.env.DATABASE_URL || 'postgres://localhost:5432/bookie',
-// 	{
-// 		dialect: 'postgres',
-// 		dialectOptions: {
-// 			ssl: process.env.DATABASE_URL
-// 				? {
-// 						require: true,
-// 						rejectUnauthorized: false,
-// 				  }
-// 				: false,
-// 		},
-// 		logging: false,
-// 	}
-// );
-
-const db = new Sequelize({
-    database: "postgres",
-    username: "postgres",
-    password: "daehanmingook",
-    host: "db.amamrmcsilwimyrszvlr.supabase.co",
-    port: 5432,
-    dialect: "postgres",
-    dialectModule: pg,
-    dialectOptions: {
-        ssl: {
-            require: true, // This will help, but cause error
-            rejectUnauthorized: false, // This will fix error
+const db = new Sequelize(
+    process.env.DATABASE_URL || "postgres://localhost:5432/bookie",
+    {
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: process.env.DATABASE_URL
+                ? {
+                      require: true,
+                      rejectUnauthorized: false,
+                  }
+                : false,
         },
-    },
-});
+        logging: false,
+    }
+);
 
 module.exports = db;
