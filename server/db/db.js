4100c09f99a80f6e2c275e7920a3454b95739d82
Danny <dannyjmargolin@gmail.com>
2023-03-12 18:13:54 -0400

    migrate to supabase

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,10 @@
-const Sequelize = require('sequelize');
-const pg = require('pg');
-
+const Sequelize = require("sequelize");
+const pg = require("pg");
 
 const config = {};
 
 if (process.env.QUIET) {
-	config.logging = false;
+    config.logging = false;
 }
 
 // const db = new Sequelize(
@@ -25,19 +24,19 @@ if (process.env.QUIET) {
 // );
 
 const db = new Sequelize({
-	database: 'fsbookie',
-	username: 'postgres',
-	password: 'password',
-	host: 'fsbookie.czw8tftookde.us-east-1.rds.amazonaws.com',
-	port: 5432,
-	dialect: 'postgres',
-	dialectModule: pg,
-	dialectOptions: {
-		ssl: {
-			require: true, // This will help, but cause error
-			rejectUnauthorized: false, // This will fix error
-		},
-	},
+    database: "postgres",
+    username: "postgres",
+    password: "daehanmingook",
+    host: "db.amamrmcsilwimyrszvlr.supabase.co",
+    port: 5432,
+    dialect: "postgres",
+    dialectModule: pg,
+    dialectOptions: {
+        ssl: {
+            require: true, // This will help, but cause error
+            rejectUnauthorized: false, // This will fix error
+        },
+    },
 });
 
 module.exports = db;
