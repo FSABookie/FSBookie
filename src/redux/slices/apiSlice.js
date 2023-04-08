ad8a70fecafb53f741377797cdecf5a3ad6bcfcd
Danny <dannyjmargolin@gmail.com>
2023-04-08 18:58:24 -0400

    api url

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "apiSlice",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://fsabookie.com/api",
+    baseUrl: "https://www.fsabookie.com/api",
     // baseUrl: "http://localhost:3000/api",
   }),
   tagTypes: [
