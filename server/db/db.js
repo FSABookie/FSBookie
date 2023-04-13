deaaef8fbf7bee1ba5c268d921fc787acc7ea0f7
Danny <dannyjmargolin@gmail.com>
2023-04-13 12:18:58 -0700

    db link

--- server/db/db.js
+++ server/db/db.js
@@ -4,23 +4,24 @@ const pg = require("pg");
 const config = {};
 
 if (process.env.QUIET) {
-    config.logging = false;
+  config.logging = false;
 }
 
 const db = new Sequelize(
-    process.env.DATABASE_URL || "postgres://localhost:5432/bookie",
-    {
-        dialect: "postgres",
-        dialectOptions: {
-            ssl: process.env.DATABASE_URL
-                ? {
-                      require: true,
-                      rejectUnauthorized: false,
-                  }
-                : false,
-        },
-        logging: false,
-    }
+  process.env.DATABASE_URL ||
+    "postgresql://postgres:daehanmingook@db.amamrmcsilwimyrszvlr.supabase.co:5432/postgres",
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: process.env.DATABASE_URL
+        ? {
+            require: true,
+            rejectUnauthorized: false,
+          }
+        : false,
+    },
+    logging: false,
+  }
 );
 
 module.exports = db;
