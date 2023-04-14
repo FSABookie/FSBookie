04a5e824e5fa497098e4ac88aab0525679abe556
Danny <dannyjmargolin@gmail.com>
2023-04-14 09:00:53 -0700

    webhooks link

--- server/db/db.js
+++ server/db/db.js
@@ -4,23 +4,23 @@ const pg = require("pg");
 const config = {};
 
 if (process.env.QUIET) {
-    config.logging = false;
+  config.logging = false;
 }
 
 const db = new Sequelize(
-    process.env.DATABASE_URL || "postgres://localhost:5432/bookie",
-    {
-        dialect: "postgres",
-        dialectOptions: {
-            ssl: process.env.DATABASE_URL
-                ? {
-                      require: true,
-                      rejectUnauthorized: false,
-                  }
-                : false,
-        },
-        logging: false,
-    }
+  process.env.DATABASE_URL || "postgres://danny:123@localhost:5432/bookie",
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: process.env.DATABASE_URL
+        ? {
+            require: true,
+            rejectUnauthorized: false,
+          }
+        : false,
+    },
+    logging: false,
+  }
 );
 
 module.exports = db;
