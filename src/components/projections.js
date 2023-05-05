5199947192a0899d78c5dbcad13a015c82ae2e22
Danny <dannyjmargolin@gmail.com>
2023-05-05 08:42:54 -0700

    projections sport type

--- src/components/projections.js
+++ src/components/projections.js
@@ -272,10 +272,10 @@ const Projections = ({ games, sport }) => {
           Baseball
         </Link>
       </SportsHeader>
-      {activeGames.sport === "NBA" && <Header>NBA</Header>}
+      {/* {activeGames.sport === "NBA" && <Header>NBA</Header>}
       {activeGames.sport === "NFL" && <Header>NFL</Header>}
       {activeGames.sport === "NHL" && <Header>NHL</Header>}
-      {activeGames.sport === "MLB" && <Header>MLB</Header>}
+      {activeGames.sport === "MLB" && <Header>MLB</Header>} */}
       <Attempt>
         <GamesContainer>
           <Games>
